21ae8e33fde0487e7cb70ed9e338b4a15880a538
ayushM <ayushmehrotra099@gmail.com>
2025-01-25 14:12:42 +0530

    reduce the font size

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -7,7 +7,7 @@ export default function Home() {
       <h1 className="mb-8 text-2xl font-semibold tracking-tighter">
         Ayush Mehrotra&apos;s &nbsp; Portfolio
       </h1>
-      <p className="mb-4 text-[1.4em] ">
+      <p className="mb-4 text-[1.2em] ">
         I&apos;m a software developer with expertise in JavaScript and its
         frameworks. I love Linux because it enhances productivity. I&apos;m 21
         years old and live in Varanasi, UP, India. I&apos;m open to new ideas,
