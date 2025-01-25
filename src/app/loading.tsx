a8a16c85bf86ef872139859930c8f00ef5a84a64
ayushM <ayushmehrotra099@gmail.com>
2025-01-25 21:33:08 +0530

    added loader

---
+++ src/app/loading.tsx
@@ -0,0 +1,10 @@
+export default function Loading() {
+  return (
+    <div className="flex items-center justify-center h-100vh">
+      <div className="hover:scale-110 transition-all ease-in-out cursor-pointer hover:shadow-xl hover:shadow-neutral-700 delay-250 animate-spin delay-50 duration-1000 bg-gradient-to-br border-4 shadow-inner shadow-neutral-700 border-neutral-950 from-white/80 to-gray-600 rounded-full grid place-items-center z-0 h-20 w-20 relative">
+        <div className="rounded-full bg-neutral-900 absolute rotate-[90deg] z-20 h-20 scale-50 w-2"></div>
+        <div className="rounded-full bg-neutral-900 absolute rotate-[180deg] z-20 h-20 scale-50 w-2"></div>
+      </div>
+    </div>
+  );
+}
