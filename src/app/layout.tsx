bb7eff9c999cabf125455c5c99ecbd183489e763
theayushmehrotra <ayush@mehrotra>
2025-03-15 21:27:44 +0530

    layout js fix

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,7 +1,6 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
-import { Footer } from "@/components/footer";
 import Navbar from "@/components/nav";
 import { baseUrl } from "./sitemap";
 
