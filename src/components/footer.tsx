a619261a697e1482101c54d9a5bdeb14f5d23223
theayushmehrotra <ayush@mehrotra>
2025-03-16 22:37:08 +0530

    ficxer vercel err

--- src/components/footer.tsx
+++ src/components/footer.tsx
@@ -1,12 +1,9 @@
 "use client";
 
-import { BackgroundBeams } from "@/components/ui/Hero";
-
 export const Footer = () => {
   return (
     <div className="  h-full w-full  ">
       <h1> Hello WOrld</h1>
-      <BackgroundBeams className="text-red-400" />
     </div>
   );
 };
