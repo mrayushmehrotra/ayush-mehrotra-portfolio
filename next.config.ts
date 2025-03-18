f89d77db34fce4ddabc26c0af9469cba5d73a2c3
theayushmehrotra <ayush@mehrotra>
2025-03-18 22:03:50 +0530

    remove mdx build

--- next.config.ts
+++ next.config.ts
@@ -1,18 +1,10 @@
 import type { NextConfig } from "next";
-import createMDX from "@next/mdx";
 
-/** @type {import('next').NextConfig} */
 const nextConfig: NextConfig = {
-  // Configure `pageExtensions` to include markdown and MDX files
-  pageExtensions: ["js", "jsx", "md", "mdx", "ts", "tsx"],
-  // Optionally, add any other Next.js config below
+  /* config options here */
   images: {
     domains: ["avatars.githubusercontent.com"],
   },
 };
 
-const withMDX = createMDX({
-  // Add markdown plugins here, as desired
-});
-
-export default withMDX(nextConfig);
+export default nextConfig;
