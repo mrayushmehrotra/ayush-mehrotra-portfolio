d5feb31f2644c108047a5bbc218ca29d12bc1bd4
Ayush Mehrotra <ayushmehrotra099@gmail.com>
2025-04-09 11:25:28 +0530

    added links in exp section

--- src/components/Experience.tsx
+++ src/components/Experience.tsx
@@ -19,6 +19,7 @@ export function Experience() {
         />
         <br />
         <h1 className="text-3xl font-[neuka] p-3">Work Experience(2)</h1>
+
         <InfiniteMovingCards items={WorkExp} direction="left" speed="normal" />
       </div>
     </>
@@ -31,6 +32,7 @@ const testimonials = [
       "Lyft Back-End Engineering Virtual Experience Program held by Forage A program that consists of 4 chapter namely software architecture refactoring unit-testing and test driven-development",
     name: "Lyft Back-End Engineering Job Simulation.",
     title: "Lyft",
+    link: "https://forage-uploads-prod.s3.amazonaws.com/completion-certificates/Lyft/xSw9echtixLAoPdsH_Lyft_Zyj746CAXJ5BbCdCK_1700289748124_completion_certificate.pdf",
   },
   {
     quote:
@@ -52,11 +54,13 @@ const WorkExp = [
       "Developed a React.js dashboard to manage records for over 150+ employees. Enhanced API performance by 25% via Redis caching and optimized React hooks. Integrated a Facebook chat plugin for directing customer care support for users. Implemented a WhatsApp event notifier system for user interactions.",
     name: "Full Stack Mern Developer",
     title: "Codetikki",
+    link: "https://codetikki.in",
   },
   {
     quote:
       "Created A Web Assemby (WASM) zip extractor that will be  integrated in the ai & will be provided to AI for context",
     name: "Software Engineer intern Aug",
     title: "Arakoo.ai",
+    link: "https://www.loom.com/share/45b2e0306d52459aa1cfe49ef28a285e?sid=abc27ab9-0813-4777-a7e0-f8ff3f2a2472",
   },
 ];
--- src/components/ui/infinite-moving-cards.tsx
+++ src/components/ui/infinite-moving-cards.tsx
@@ -14,6 +14,7 @@ export const InfiniteMovingCards = ({
     quote: string;
     name: string;
     title: string;
+    link?: string;
   }[];
   direction?: "left" | "right";
   speed?: "fast" | "normal" | "slow";
@@ -90,25 +91,27 @@ export const InfiniteMovingCards = ({
             className="relative w-[350px] max-w-full shrink-0 rounded-2xl border border-b-0 border-zinc-200 bg-[linear-gradient(180deg,#fafafa,#f5f5f5)] px-8 py-6 md:w-[450px] dark:border-zinc-700 dark:bg-[linear-gradient(180deg,#27272a,#18181b)]"
             key={item.name}
           >
-            <blockquote>
-              <div
-                aria-hidden="true"
-                className="user-select-none pointer-events-none absolute -top-0.5 -left-0.5 -z-1 h-[calc(100%_+_4px)] w-[calc(100%_+_4px)]"
-              ></div>
-              <span className="relative z-20 text-sm leading-[1.6] font-normal text-neutral-800 dark:text-gray-100">
-                {item.quote}
-              </span>
-              <div className="relative z-20 mt-6 flex flex-row items-center">
-                <span className="flex flex-col gap-1">
-                  <span className="text-sm leading-[1.6] font-normal text-neutral-500 dark:text-gray-400">
-                    {item.name}
-                  </span>
-                  <span className="text-sm leading-[1.6] font-normal text-neutral-500 dark:text-gray-400">
-                    {item.title}
-                  </span>
+            <a href={item.link} target="_blank">
+              <blockquote>
+                <div
+                  aria-hidden="true"
+                  className="user-select-none pointer-events-none absolute -top-0.5 -left-0.5 -z-1 h-[calc(100%_+_4px)] w-[calc(100%_+_4px)]"
+                ></div>
+                <span className="relative z-20 text-sm leading-[1.6] font-normal text-neutral-800 dark:text-gray-100">
+                  {item.quote}
                 </span>
-              </div>
-            </blockquote>
+                <div className="relative z-20 mt-6 flex flex-row items-center">
+                  <span className="flex flex-col gap-1">
+                    <span className="text-sm leading-[1.6] font-normal text-neutral-500 dark:text-gray-400">
+                      {item.name}
+                    </span>
+                    <span className="text-sm leading-[1.6] font-normal text-neutral-500 dark:text-gray-400">
+                      {item.title}
+                    </span>
+                  </span>
+                </div>
+              </blockquote>
+            </a>
           </li>
         ))}
       </ul>
