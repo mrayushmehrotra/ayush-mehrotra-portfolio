0144199b24379a722c0dd5ecb0c3491dcae55230
Ayush Mehrotra <ayushmehrotra099@gmail.com>
2025-05-01 11:52:07 +0530

    fixed responsiveness

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,7 +3,6 @@ import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import { baseUrl } from "./sitemap";
 import LoaderWrapper from "@/components/LoaderWrapper";
-import Image from "next/image";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
